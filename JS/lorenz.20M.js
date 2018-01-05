6d725a8a4678fdff17082ff2e5a3c5c80f99ac28
Chachay <t.sasai0802@gmail.com>
2018-01-06 00:27:33 +0900

    Add JS (node.js)

---
+++ JS/lorenz.20M.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const lorenz = () => {
+    const dt = 0.01;
+    const n  = 20000000;
+
+    const a = 10.;
+    const b = 28.;
+    const c = 8/3;
+
+    let x = new Float64Array(n);
+    let y = new Float64Array(n);
+    let z = new Float64Array(n);
+
+    x[0] = 0;
+    y[0] = 1.;
+    z[0] = 1.05;
+
+    for(let i = 0; i < n; i++){
+        x[i+1] = x[i] + dt * (- a    * x[i] + a * y[i]);
+        y[i+1] = y[i] + dt * (- x[i] * z[i] + b * x[i] - y[i]);
+        z[i+1] = z[i] + dt * ( x[i]  * y[i] - c * z[i] );
+    }
+    return [x,y,z];
+};
+
+let X = lorenz();
